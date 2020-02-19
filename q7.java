4bf449038e5d98a9e8bee115182cdad9009a34f1
abhileshmalik <60641088+abhileshmalik@users.noreply.github.com>
2020-02-19 15:27:17 +0530

    Add files via upload

---
+++ q7.java
@@ -0,0 +1,17 @@
+public class q7 {
+    static {
+        String l_name="Malik";
+        System.out.println("last name = "+l_name);
+    }
+    static int age=21;
+    static String f_name;
+    static void fun1(){
+        f_name="Abhilesh";
+    }
+    public static void main(String[] args) {
+        System.out.println("age = "+age);
+        q7.fun1();
+        System.out.println("first name = "+f_name);
+    }
+}
+

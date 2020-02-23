6310b79086c378c9d600488c8840a0886d92d3bd
abhileshmalik <60641088+abhileshmalik@users.noreply.github.com>
2020-02-23 20:49:36 +0530

    Q10

---
+++ SpecialStack.java
@@ -0,0 +1,55 @@
+package assignment1.Collections;
+
+import java.util.Stack;
+
+class SpecialStack extends Stack<Integer>
+{
+    Stack<Integer> min = new Stack<>();
+
+    void push(int x)
+    {
+        if(isEmpty() == true)
+        {
+            super.push(x);
+            min.push(x);
+        }
+        else
+        {
+            super.push(x);
+            int y = min.pop();
+            min.push(y);
+            if(x < y)
+                min.push(x);
+            else
+                min.push(y);
+        }
+    }
+
+    public Integer pop()
+    {
+        int x = super.pop();
+        min.pop();
+        return x;
+    }
+
+    int getMin()
+    {
+        int x = min.pop();
+        min.push(x);
+        return x;
+    }
+
+    public static void main(String[] args)
+    {
+        SpecialStack s = new SpecialStack();
+        s.push(15);
+        s.push(20);
+        s.push(30);
+        s.push(55);
+        s.push(90);
+        s.push(40);
+        System.out.println(s.getMin());
+        s.push(11);
+        System.out.println(s.getMin());
+    }
+}
